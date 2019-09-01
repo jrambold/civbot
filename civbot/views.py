495ea4d008ba733944c14584c06e28be4b20c681
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2019-09-01 07:57:48 -0600

    handles json payload for civ games

--- civbot/views.py
+++ civbot/views.py
@@ -11,12 +11,18 @@ def root(request):
 
 @csrf_exempt
 def index(request):
-    info = json.dumps(request.POST)
+    info = json.loads(request.body)
+
+    try:
+        message = "Hey @" + info['value2'] + " it's your turn in " + info['value1'] + ". Turn:" + info['value3']
+    except:
+        message = "Cibot Error"
+
     client = slack.WebClient(token=settings.SLACK_CIVBOT)
     starterbot_id = None
     response = client.chat_postMessage(
-        channel='#bot-testing',
-        text=info)
-    assert response["ok"]
-    assert response["message"]["text"] == info
-    return JsonResponse(json.loads(info))
+        channel='#civilization',
+        text=message)
+    # assert response["ok"]
+    # assert response["message"]["text"] == info
+    return JsonResponse(info)
