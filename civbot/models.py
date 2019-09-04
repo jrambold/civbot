5cff1f20c2db5111b6981a99876c52127ed327af
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2019-09-04 08:21:32 -0600

    models added

--- civbot/models.py
+++ civbot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    player = models.CharField(max_length=200)
+    turn = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    steamName = models.CharField(max_length=200)
+    slackNotification = models.BooleanField(default=True)
+    slackId = models.CharField(max_length=200, blank=True)
+    discordNotification = models.BooleanField(default=False)
+    discordId = models.CharField(max_length=200, blank=True)
+    emailNotification = models.BooleanField(default=False)
+    email = models.EmailField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
--- civbot/views.py
+++ civbot/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from civbot.models import Game, Player
 import os
 import slack
 import json
-from .userlist import getSlackId
+from .notifications import getSlackId
 
 def root(request):
     return HttpResponse("Hello World")
@@ -15,18 +16,31 @@ def index(request):
     info = json.loads(request.body)
 
     try:
-        name = info['value2']
+        player = info['value2']
         game = info['value1']
         turn = info['value3']
+    except:
+        message = info
+        client = slack.WebClient(token=settings.SLACK_CIVBOT)
+        response = client.chat_postMessage(
+            channel='#civilization',
+            text=message)
+        return JsonResponse(info)
 
-        name = getSlackId(name)
+    game_player = Game.objects.filter(name = game, player = player)
+    name = models.CharField(max_length=200)
+    player = models.CharField(max_length=200)
+    player = getSlackId(player)
+    message = "Hey " + player + " it's your turn in " + game + ".\nTurn: " + turn
 
-        message = "Hey " + name + " it's your turn in " + game + ".\nTurn: " + turn
+
+
+
+
     except:
         message = info
 
     client = slack.WebClient(token=settings.SLACK_CIVBOT)
-    starterbot_id = None
     response = client.chat_postMessage(
         channel='#civilization',
         text=message)
