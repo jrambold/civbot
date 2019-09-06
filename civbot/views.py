d35d8174791a4d5af81b4caa477beb1c31ed9672
jrambo531 <jake_rambold@comcast.com>
2019-09-06 15:27:55 -0600

    nevermind it broke

--- civbot/views.py
+++ civbot/views.py
@@ -38,28 +38,23 @@ def index(request):
 
 @csrf_exempt
 def command(request):
+    response = {}
     slackCommand = parse_qs(request.body.decode('utf-8', "ignore"))
 
-    response = {}
-    response["response_type"] = "ephemeral"
-    response["text"] = str(slackCommand)
+    text = slackCommand['text'][0].split(' ')
 
-    # text = slackCommand['text'].split(' ')
-    #
-    # response = {}
-    #
-    # if text[0] == 'help':
-    #     response["response_type"] = "ephemeral"
-    #     response["text"] = "I'm still building it asshole. Hold your horses"
-    #     response["attachments"] = [
-    #                                 {"text":"help - How do you think you got here?"},
-    #                                 {"text":"game [gamename] - Tells you info about the game"}
-    #                               ]
-    # elif text[0] == 'game':
-    #     response["response_type"] = "in_channel"
-    #     response["text"] = "I'm still building it asshole. Hold your horses"
-    # else:
-    #     response["response_type"] = "ephemeral"
-    #     response["text"] = "Not a command. User error. User meaning you <@" + slackCommand['user_id'] + '>!'
+    if text[0] == 'help':
+        response["response_type"] = "ephemeral"
+        response["text"] = "I'm still building it asshole. Hold your horses"
+        response["attachments"] = [
+                                    {"text":"help - How do you think you got here?"},
+                                    {"text":"game [gamename] - Tells you info about the game"}
+                                  ]
+    elif text[0] == 'game':
+        response["response_type"] = "in_channel"
+        response["text"] = "I'm still building it asshole. Hold your horses"
+    else:
+        response["response_type"] = "ephemeral"
+        response["text"] = "Not a command. User error. User meaning you <@" + slackCommand['user_id'][0] + '>!'
 
     return JsonResponse(response)
