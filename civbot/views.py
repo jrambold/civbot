e95c4a56d200bdd8374263745cf19c878da7f77c
jrambo531 <jake_rambold@comcast.com>
2019-09-06 15:16:37 -0600

    working commands

--- civbot/views.py
+++ civbot/views.py
@@ -44,17 +44,17 @@ def command(request):
     response = {}
 
     if text[0] == 'help':
-        response["response_type": "ephemeral"]
+        response["response_type"] = "ephemeral"
         response["text"] = "I'm still building it asshole. Hold your horses"
         response["attachments"] = [
                                     {"text":"help - How do you think you got here?"},
                                     {"text":"game [gamename] - Tells you info about the game"}
                                   ]
     elif text[0] == 'game':
-        response["response_type": "in_channel"]
+        response["response_type"] = "in_channel"
         response["text"] = "I'm still building it asshole. Hold your horses"
     else:
-        response["response_type": "ephemeral"]
+        response["response_type"] = "ephemeral"
         response["text"] = "Not a command. User error. User meaning you <@" + slackCommand['user_id'] + '>!'
-        
+
     return JsonResponse(response)
