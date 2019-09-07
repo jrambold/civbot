4d27c9dd97904c53f61ceec398ae2efe22c428df
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2019-09-07 07:35:21 -0600

    token issues

--- civbot/views.py
+++ civbot/views.py
@@ -51,7 +51,7 @@ def command(request):
         return HttpResponse('Invalid Request')
 
     if slackCommand['token'] != settings.SLACK_TOKEN:
-        return HttpResponse(settings.SLACK_TOKEN)
+        return HttpResponse(slackCommand['token'] + ' - ' + settings.SLACK_TOKEN)
 
     if text[0] == 'help':
         response = interact.help()
