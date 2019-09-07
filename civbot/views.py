92634cc7a43e53d6589f7ada8eac80a4b9b35583
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2019-09-06 21:22:30 -0600

    gamelist string generation

--- civbot/views.py
+++ civbot/views.py
@@ -64,7 +64,7 @@ def command(request):
 
     elif text[0] == 'gamelist':
         game_list = "Current Games: \n"
-        game_query = Game.objects.all()order_by('-updated')
+        game_query = Game.objects.all().order_by('-updated')
         # for game in game_query:
         #     game_list = game_list + '1' #game.name + ' Turn: ' + str(game.turn) + ' Last Played on: \n'# + game.updated.strftime("%m/%d/%Y, %H:%M") + '\n'
         thing = str(game_query.count())
