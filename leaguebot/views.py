b46be0185d8a449b418e4ec10e663a4681220464
jrambold <jrambold@gmail.com>
2019-09-29 11:52:32 -0600

    rank response

--- leaguebot/views.py
+++ leaguebot/views.py
@@ -43,9 +43,6 @@ def command(request):
             response["text"] = "Must supply a username"
 
     elif text[0] == 'stats':
-        response["text"] = "Test Spot 0"
-        response["response_type"] = "ephemeral"
-        return JsonResponse(response)
         if len(text) > 1 and len(text[1]) > 0:
             response = interact.stats(text[1])
         else:
