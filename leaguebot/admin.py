332e59758319e958ea24f5eb6d335f8e961c8d8e
Rambold <jrambo531@cable.comcast.com>
2019-09-30 08:16:45 -0600

    champion datapull

--- leaguebot/admin.py
+++ leaguebot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Player, FlexMatch, SoloMatch, Rank
+from .models import Player, FlexMatch, SoloMatch, Rank, Champion
 
+admin.site.register(Champion)
 admin.site.register(Player)
 admin.site.register(Rank)
 admin.site.register(SoloMatch)
